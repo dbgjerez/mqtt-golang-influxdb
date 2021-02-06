d8bea3b32650b1913358cd62ce7a38043f6bb373
David Borrego <dbgjerez@gmail.com>
2021-02-06 19:25:44 +0100

    healcheck

--- controllers/HealthController.go
+++ controllers/HealthController.go
@@ -1,12 +1,31 @@
 package controllers
 
 import (
+	"mqtt-golang-subscriber/adapter"
+	"mqtt-golang-subscriber/db"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type HealthController struct{}
+type HealthController struct {
+}
+
+type Health struct {
+	Status string `json:"status"`
+}
+
+func HealthControllerHandler(mqttConn *adapter.MqttConnection, influxConn *db.InfluxDBConnection) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		h := Health{}
+		if mqttConn.IsConnected() && influxConn.IsConnected() {
+			h.Status = "UP"
+		} else {
+			h.Status = "DOWN"
+		}
+		c.JSON(http.StatusOK, h)
+	}
+}
 
 func (h *HealthController) Default(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "UP"})
