d76fa0fd206ca89a44aece80e70374eed19c09eb
David Borrego <dbgjerez@gmail.com>
2021-02-06 19:31:55 +0100

    comments

--- app.go
+++ app.go
@@ -12,19 +12,15 @@ import (
 func main() {
 	router := gin.Default()
 
+	influxDBConnection := db.NewConnection()
+
 	mqttConnection := adapter.NewConnection(os.Getenv(adapter.MqttClientName))
 	mqttConnection.Subscribe(os.Getenv(adapter.MqttTopicName))
-
-	influxDBConnection := db.NewConnection()
-	//influxDBConnection.Insert()
 
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/health", controllers.HealthControllerHandler(mqttConnection, influxDBConnection))
 	}
-
-	//influxDBConnection := db.NewConnection()
-	//influxDBConnection.Insert()
 
 	router.NoRoute(func(c *gin.Context) {
 		c.JSON(404, gin.H{"msg": "Not found"})
