0f715057cf4685dd56022c01f336b79790b56dd9
David Borrego <dbgjerez@gmail.com>
2021-02-07 12:10:50 +0100

    int to int64

--- models/ChipEvent.go
+++ models/ChipEvent.go
@@ -4,7 +4,7 @@ type ChipEvent struct {
 	Chip    string `json:"chip"`
 	Sensors []struct {
 		Sensor   string `json:"sensor"`
-		Time     int    `json:"time"`
+		Time     int64  `json:"time"`
 		Humidity int    `json:"humidity"`
 	} `json:"sensors"`
 }
